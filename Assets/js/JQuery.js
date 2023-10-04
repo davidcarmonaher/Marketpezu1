e64b0b05681fe3b75425dc3de0b508e8a87b5b0a
raulch145 <144687416+raulch145@users.noreply.github.com>
2023-10-04 21:49:37 +0200

    Add files via upload

---
+++ Assets/js/JQuery.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+    $("#index").on( "click", function() {
+    $('#pagindex').show();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+
+    });
+}); 
+$(function(){    
+    $("index").on("click", function(){
+    $('#pagindex').show();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("index").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').show();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("index").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').show();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("index").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').show();
+    });
+});
+
+$(function(){    
+    $("secundaria").on("click", function(){
+    $('#pagindex').show();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("secundaria").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').show();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("secundaria").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').show();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("secundaria").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').show();
+    });
+});
+
+$(function(){    
+    $("contacto").on("click", function(){
+    $('#pagindex').show();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("contacto").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').show();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("contacto").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').show();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("contacto").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').show();
+    });
+});
+
+$(function(){    
+    $("registro").on("click", function(){
+    $('#pagindex').show();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("registro").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').show();
+    $('#pagcontacto').hide();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("registro").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').show();
+    $('#pagregistro').hide();
+    });
+});
+$(function(){    
+    $("registro").on("click", function(){
+    $('#pagindex').hide();
+    $('#pagsecundaria').hide();
+    $('#pagcontacto').hide();
+    $('#pagregistro').show();
+    });
+});
